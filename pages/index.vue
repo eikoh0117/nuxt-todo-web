e6277c942db9930f78d3adc47cfca76e39a35030
Hidemitsu Aoki <olympos0117@gmail.com>
2020-06-30 23:47:56 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AddTodo />
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import AddTodo from "~@components/AddTodo.vue";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  components: {
+    AddTodo,
+    TodoList
+  },
+  data() {
+    return {
+      todos: []
+    };
+  }
+};
+</script>
