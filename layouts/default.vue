a7a3d4994dd229e80a5b0e981c15a2f64b4b27d3
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-01 06:15:20 +0900

    Different header depend on status of authentication

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -15,7 +27,7 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
@@ -44,8 +56,8 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
@@ -57,23 +69,45 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/"
-        },
-        {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire"
-        }
-      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: "Vuetify.js"
+      title: "Todo App" //追加
     };
+  },
+  computed: {
+    user() {
+      return this.$store.state.currentUser;
+    },
+    items() {
+      if (this.user) {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "Todos",
+            to: "/"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "mypage",
+            to: "/mypage"
+          }
+        ];
+      } else {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "ログイン",
+            to: "/login"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "新規登録",
+            to: "/signup"
+          }
+        ];
+      }
+    }
   }
 };
 </script>
